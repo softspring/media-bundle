dc6ff824092cf1562e9eb8d88d7a513bed4ea37f
Javi H. Gil <javihgil@softspring.es>
2023-02-01 15:19:25 +0100

    Update php cs fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -9,6 +9,7 @@ $config = new PhpCsFixer\Config();
     return $config->setRules([
         '@Symfony' => true,
         'full_opening_tag' => false,
+        'phpdoc_separation' => false,
     ])
     ->setFinder($finder)
 ;
