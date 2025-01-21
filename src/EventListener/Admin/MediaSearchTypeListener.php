d800ae206faba8755ecafe3e8745f02661a7b729
Javi H. Gil <javihgil@users.noreply.github.com>
2025-01-21 10:02:43 +0100

    Media search type listener filters by valid_types parameter (#82)

--- src/EventListener/Admin/MediaSearchTypeListener.php
+++ src/EventListener/Admin/MediaSearchTypeListener.php
@@ -2,6 +2,7 @@
 
 namespace Softspring\MediaBundle\EventListener\Admin;
 
+use Softspring\Component\CrudlController\Event\FilterEvent;
 use Softspring\MediaBundle\SfsMediaEvents;
 
 class MediaSearchTypeListener extends AbstractMediaListener
@@ -14,11 +15,25 @@ class MediaSearchTypeListener extends AbstractMediaListener
             // SfsMediaEvents::ADMIN_MEDIAS_SEARCH_TYPE_INITIALIZE => [],
             // SfsMediaEvents::ADMIN_MEDIAS_SEARCH_TYPE_FILTER_FORM_PREPARE => [],
             // SfsMediaEvents::ADMIN_MEDIAS_SEARCH_TYPE_FILTER_FORM_INIT => [],
-            // SfsMediaEvents::ADMIN_MEDIAS_SEARCH_TYPE_FILTER => [],
+            SfsMediaEvents::ADMIN_MEDIAS_SEARCH_TYPE_FILTER => [
+                ['onFilterSearchType', 0],
+            ],
             SfsMediaEvents::ADMIN_MEDIAS_SEARCH_TYPE_VIEW => [
                 ['onViewAddMediaTypes', 0],
             ],
             // SfsMediaEvents::ADMIN_MEDIAS_SEARCH_TYPE_EXCEPTION => [],
         ];
+    }
+
+    public function onFilterSearchType(FilterEvent $event): void
+    {
+        $filters = $event->getFilters();
+
+        $validTypes = array_filter(explode(',', $event->getRequest()->get('valid_types')));
+        if (!empty($validTypes) && empty($filters['type__in'])) {
+            $filters['type__in'] = $validTypes;
+        }
+
+        $event->setFilters($filters);
     }
 }
