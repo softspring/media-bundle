9bc1ac8f9f1ee7d366d20afe9b26546cb17667c7
Javi H. Gil <javihgil@softspring.es>
2026-02-02 13:56:20 +0100

    Update for SF8

--- rector.php
+++ rector.php
@@ -2,20 +2,20 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\Set\ValueObject\SetList;
 use Rector\Symfony\Set\SymfonySetList;
-use Rector\Symfony\Set\SensiolabsSetList;
+use Rector\ValueObject\PhpVersion;
 
 return RectorConfig::configure()
-    ->withImportNames(removeUnusedImports: true)
-    ->withPaths([
-        __DIR__ . '/src',
-    ])
+    ->withPaths(array_merge(
+is_dir(__DIR__ . '/src') ? [__DIR__ . '/src'] : [],
+        is_dir(__DIR__ . '/tests') ? [__DIR__ . '/tests'] : []
+    ))
     ->withSets([
-        DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
-        SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
-//        SensiolabsSetList::ANNOTATIONS_TO_ATTRIBUTES,
-//        SymfonySetList::SYMFONY_62,
-//        SymfonySetList::SYMFONY_CODE_QUALITY,
-//        SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-    ]);
+        SymfonySetList::SYMFONY_80,
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::TYPE_DECLARATION,
+    ])
+    ->withPhpVersion(PhpVersion::PHP_84)
+    ->withComposerBased(symfony: true);
