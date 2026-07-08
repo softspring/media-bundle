3df08ab2c28f714b88710714a0f4e330ba402a33
Javi H. Gil <javihgil@users.noreply.github.com>
2026-07-08 09:09:13 +0200

    Escape image attributes (#127)
    (cherry picked from commit 9e9a848e4b3c05a476cd39cfcb6c4896d8189690)

--- src/Render/MediaRenderer.php
+++ src/Render/MediaRenderer.php
@@ -177,7 +177,7 @@ class MediaRenderer
     protected function htmlAttributes(array $attributes): string
     {
         array_walk($attributes, function (&$value, $attribute): void {
-            $value = "$attribute=\"$value\"";
+            $value = sprintf('%s="%s"', $attribute, htmlspecialchars((string) $value, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8'));
         });
 
         return implode(' ', $attributes);
